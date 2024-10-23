9d75c73e8c3d193d77db843eec998e0f036d80b4
MatteoGozzi00 <mehvahdjukaar@gmail.com>
2024-10-23 11:08:30 +0200

    Update SimpleAdder.java

--- SimpleAdder.java
+++ SimpleAdder.java
@@ -4,6 +4,7 @@ public class SimpleAdder {
     }
     
 
+    
     public static void main(String[] args) {
         SimpleAdder adder = new SimpleAdder();
         System.out.println("Test: " + (adder.add(3, 4) == 7));
