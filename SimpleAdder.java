5352ebe3744eb9d6308762121c5d78f60a1a2039
MatteoGozzi00 <mehvahdjukaar@gmail.com>
2024-10-23 11:06:43 +0200

    Update SimpleAdder.java
    q

--- SimpleAdder.java
+++ SimpleAdder.java
@@ -2,6 +2,7 @@ public class SimpleAdder {
     public int add(int a, int b) {
         return a + b;
     }
+    
 
     public static void main(String[] args) {
         SimpleAdder adder = new SimpleAdder();
